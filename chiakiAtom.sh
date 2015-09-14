9d8aaf18f9bce9e850de464aec2e5c3709ca24d8
Muhammad Aliff Muazzam (Tester2009) <alepcat1710@users.noreply.github.com>
2015-09-14 11:02:24 +0800

    Update chiakiAtom.sh

--- chiakiAtom.sh
+++ chiakiAtom.sh
@@ -1,34 +1,34 @@
-#!/bin/sh
-echo "chiakiAtom 0.1v"
-echo "Backup your file as soon as possible !"
-echo "Written by Muhammad Aliff Muazzam @Tester2009"
-echo "https://www.facebook.com/Tester2009\nhttps://github.com/alepcat1710\nhttp://aliff.muazzam.my\n"
+# Written by Muhammad Aliff Muazzam (Tester2009)
+# https://facebook.com/Tester2009
+# https://github.com/alepcat1710
+# Date: July 28, 2015
+# Updated: September 14, 2015. 0.2v
+# Tested on: Ubuntu 14.04 LTS
+# Feel free to use. Do not change copyright, mastah !
+
+chiakiVer="chiakiAtom 0.2v"
+echo "	$chiakiVer"
+echo "	Backup your file as soon as possible !"
+echo "	Written by Muhammad Aliff Muazzam @Tester2009"
+echo "	https://www.facebook.com/Tester2009\nhttps://github.com/alepcat1710\nhttp://aliff.muazzam.my\n"
 if [ $# -eq 0 ]; then
-	echo "Input not complete !"
-	echo "Change the variable and run !"
-	echo " "
-	echo "$0 ~/inputDirectory foldername zipname"
+	echo "	Input not complete !"
+	echo "	Change the variable and run !"
+	echo "	"
+	echo "	$0 ~/inputDirectory zipname"
 	exit 1
 fi
 
 if [ -z "$2" ]; then
-        echo "Input not complete !"
-        echo "Change the variable and run !"
-        echo " "
-        echo "$0 ~/inputDirectory foldername zipname"
+        echo "	Input not complete !"
+        echo "	Change the variable and run !"
+        echo "	"
+        echo "	$0 ~/inputDirectory zipname"
 	exit 1
 fi
 
-if [ -z "$3" ]; then
-        echo "Input not complete !"
-        echo "Change the variable and run !"
-        echo " "
-        echo "$0 ~/inputDirectory foldername zipname"
-        exit 1
-fi
-
-
-zip -r $3.zip $1;
-mkdir $2;
-cp $3.zip $2;
-echo "Done !\nThanks for using !";
+zip -r $2-$(date +%Y%m%d).zip $1;
+echo "	"
+echo "	$chiakiVer completed !"
+echo "	Your file name: $2-$(date +%Y%m%d).zip";
+echo "	Thanks for using ! ;)"
